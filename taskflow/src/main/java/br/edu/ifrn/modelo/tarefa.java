2fcbc911dc6d1f08766242016e587387efffaf1b
Lara ranielly <raniellylara951@gmail.com>
2026-05-29 11:35:13 -0300

    implementação dos requisitos funcionais e estrutura do projeto

---
+++ taskflow/src/main/java/br/edu/ifrn/modelo/tarefa.java
@@ -0,0 +1,24 @@
+package br.edu.ifrn.modelo;
+
+public class tarefa {
+    private String titulo;
+    private boolean concluida;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public boolean isConcluida() {
+        return concluida;
+    }
+
+    public void setConcluida(boolean concluida) {
+        this.concluida = concluida;
+    }
+}
+
+
---
+++ taskflow/src/main/java/br/edu/ifrn/servico/tarefaservico.java
@@ -0,0 +1,11 @@
+package br.edu.ifrn.servico;
+
+import br.edu.ifrn.modelo.tarefa;
+
+public class tarefaservico {
+
+    public void cadastrarTarefa(tarefa tarefa) {
+        System.out.println("Tarefa cadastrada!");
+    }
+
+}
